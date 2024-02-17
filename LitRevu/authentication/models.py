7fc0bbb68869add18687a2e8c27767b750384973
Mahmoud <motive007tech@gmail.com>
2024-02-17 03:12:57 +0100

    Create the Login page

---
+++ LitRevu/authentication/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+
+    CREATOR = 'CREATOR'
+    SUBSCRIBER = 'SUBSCRIBER'
+
+    ROLE_CHOICES = (
+        (CREATOR, 'Creator'),
+        (SUBSCRIBER, 'SUBSCRIBER')
+    )
+
+    profile_photo = models.ImageField()
+    role = models.CharField(max_length=30, choices=ROLE_CHOICES)
+
+
