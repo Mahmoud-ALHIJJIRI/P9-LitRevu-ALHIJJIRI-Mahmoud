9fbdd199ee46803c5e7dbb597a66b2328028a5c3
Mahmoud <motive007tech@gmail.com>
2024-02-21 14:11:30 +0100

    Working on the Blog application

--- LitRevu/blog/forms.py
+++ LitRevu/blog/forms.py
@@ -1,5 +1,10 @@
 from django import forms
+from django.urls import reverse_lazy
+from . import models
 
-class CreateTicketForm(forms.ModelForm):
+
+class TicketForm(forms.ModelForm):
     class Meta:
-        model =
+        model = models.Ticket
+        fields = ["title", "description", "image"]
+        success_url = reverse_lazy("blog:flux")
--- LitRevu/blog/views.py
+++ LitRevu/blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-
+from . import forms
+from .models import Ticket
 
 @login_required
 def home(request):
@@ -8,4 +9,32 @@ def home(request):
 
 
 def create_ticket(request):
-    pass
+    if request.method == "POST":
+        # Create an instance of the ticket form with the submitted data
+        form = forms.TicketForm(request.POST, request.FILES)
+        if form.is_valid():
+            ticket = form.save(commit=False)
+            # Set the user for the ticket
+            ticket.user = request.user
+            ticket.save()
+
+            messages.success(request, "Ticket créé avec succès.")
+
+            return redirect("blog:flux")
+        else:
+            messages.error(
+                request,
+                "Erreur lors de la création du ticket. Veuillez vérifier le formulaire.",
+            )
+    else:
+        # If the request is a GET request (the user is loading the page),
+        # create an instance of an empty ticket form
+        form = forms.TicketForm()
+
+    # Pass the ticket form to the template
+    context = {
+        "form": form,
+    }
+
+    return render(request, "blog/create_ticket.html", context)
+
