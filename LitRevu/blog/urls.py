8edb28a36521e049be98f5cf464f48dd3c8a3b75
Mahmoud <motive007tech@gmail.com>
2024-02-23 10:21:27 +0100

    The ticket creation is ready and tested

--- LitRevu/blog/urls.py
+++ LitRevu/blog/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('home/', views.home, name='home'),
     path('create_ticket/', views.create_ticket, name="create_ticket"),
+    path("create_ticket/", views.create_ticket, name="create-ticket"),
 ]
--- LitRevu/blog/views.py
+++ LitRevu/blog/views.py
@@ -1,13 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from . import forms
-from .models import Ticket
+from django.contrib import messages
+
 
 @login_required
 def home(request):
     return render(request, 'blog/home.html')
 
 
+@login_required
 def create_ticket(request):
     if request.method == "POST":
         # Create an instance of the ticket form with the submitted data
