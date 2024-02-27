4dd76e561205b5045c7df6dc2773bf8f668d4678
Mahmoud <motive007tech@gmail.com>
2024-02-27 09:41:40 +0100

    working on review ticket

--- LitRevu/blog/forms.py
+++ LitRevu/blog/forms.py
@@ -8,3 +8,23 @@ class TicketForm(forms.ModelForm):
         model = models.Ticket
         fields = ["title", "description", "image"]
         success_url = reverse_lazy('blog:home')
+
+
+class ReviewForm(forms.ModelForm):
+    class Meta:
+        model = models.Review
+        fields = ["headline", "body", "rating"]
+        success_url = reverse_lazy("blog:flux")
+
+    # Set rating field to not require
+    rating = forms.IntegerField(
+        required=False,
+        widget=forms.RadioSelect(choices=[(i, str(i)) for i in range(6)]),
+    )
+
+
+class FollowUserForm(forms.ModelForm):
+    class Meta:
+        model = models.UserFollows
+        fields = "__all__"
+        success_url = reverse_lazy("blog:subscriptions")
